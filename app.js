ae429c7427576e227f11fa07043a2c3daa6402f0
Ibrahim AdDandan <ibrahim.ad.dandan@gmail.com>
2018-01-03 22:04:20 +0200

    Add static files

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.set('port', 7777);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/status', function(req, res){
+    res.send('every thing is good :)');
+    console.log('working!');
+});
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log('start listening to the port ' + port);
+});
